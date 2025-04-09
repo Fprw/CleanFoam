4fbd19a2588fc8572556f1d8d557ef415551f750
CF <97642783+Fprw@users.noreply.github.com>
2025-04-09 17:34:32 +0300

    Create session_utils.py

---
+++ utils/session_utils.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show_admin_dashboard():
+    st.title("لوحة تحكم الأدمن")
+    st.success("مرحبًا بك، أدمن")
+
+def show_cashier_dashboard():
+    st.title("واجهة الكاشير")
+    st.info("ابدأ البيع من القائمة الجانبية")
