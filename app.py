750b4f566668a2b33d76cd33f0eb8a778c861aef
CF <97642783+Fprw@users.noreply.github.com>
2025-04-09 17:31:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
 import streamlit as st
-st.title('CleanFoam')
+from utils.auth_utils import login_user
+from utils.session_utils import show_admin_dashboard, show_cashier_dashboard
+
+st.set_page_config(page_title="CleanFoam", page_icon="🧼", layout="wide")
+
+if 'authenticated' not in st.session_state:
+    st.session_state.authenticated = False
+if 'role' not in st.session_state:
+    st.session_state.role = None
+if 'username' not in st.session_state:
+    st.session_state.username = ""
+
+if not st.session_state.authenticated:
+    login_user()
+else:
+    if st.session_state.role == "admin":
+        show_admin_dashboard()
+    elif st.session_state.role == "cashier":
+        show_cashier_dashboard()
+    else:
+        st.error("الصلاحية غير معروفة.")
